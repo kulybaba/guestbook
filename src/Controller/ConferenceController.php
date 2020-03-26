f3301c108d6c0e6a1d7c5c0e03aaff15e3f61897
Petro Kulybaba <ahurtep@gmail.com>
2020-03-26 20:36:07 +0200

    configuring DB

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConferenceController extends AbstractController
+{
+    /**
+     * @Route("/conference", name="conference")
+     */
+    public function index()
+    {
+        return $this->render('conference/index.html.twig');
+    }
+}
